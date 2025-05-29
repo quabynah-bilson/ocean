f4098d18e7a8fa778a0328a4d48b5b2dcb321836
quabynah-bilson <84394923+quabynah-bilson@users.noreply.github.com>
2025-05-29 10:43:09 +0000

    submission first part

--- integrations/github/integration/clients/github.py
+++ integrations/github/integration/clients/github.py
@@ -54,6 +54,7 @@ class IntegrationClient:
         params: Optional[dict[str, Any]] = None,
         json_data: Optional[dict[str, Any]] = None,
         method: str = "GET",
+        values_key: Optional[str] = None,
     ) -> list[dict[str, Any]]:
         """Send a request to Bitbucket v2 API with error handling."""
         logger.info(f"Sending request to {url}")
@@ -64,6 +65,8 @@ class IntegrationClient:
             )
             logger.info(f"Response from API: {response}")
             response.raise_for_status()
+            if values_key is not None:
+                return response.json().get(values_key, [])
             return response.json()
 
         except HTTPStatusError as e:
@@ -84,6 +87,7 @@ class IntegrationClient:
         url: str,
         params: Optional[dict[str, Any]] = None,
         method: str = "GET",
+        values_key: Optional[str] = None,
     ) -> AsyncGenerator[list[dict[str, Any]], None]:
         """handles HTTP calls to the API server"""
         page = 1
@@ -100,7 +104,7 @@ class IntegrationClient:
                 try:
                     logger.info(f"Fetching page {params.get("page")}")
                     response = await self._send_api_request(
-                        method=method, url=url, params=params
+                        method=method, url=url, params=params, values_key=values_key
                     )
                     logger.info(f"Fetched {len(response)} data from {url}")
                     yield response
@@ -122,5 +126,55 @@ class IntegrationClient:
 
     @cache_iterator_result()
     async def get_repositories(self) -> AsyncGenerator[list[dict[str, Any]], None]:
+        """get all repos for the current user"""
         async for repos in self._fetch_data(f"{self.base_url}/user/repos"):
             yield repos
+
+    @cache_iterator_result()
+    async def get_issues(
+        self,
+        repo_slug: str,
+    ) -> AsyncGenerator[list[dict[str, Any]], None]:
+        """get all issues for a repo"""
+        params = {
+            "per_page": DEFAULT_PAGE_SIZE,
+            "page": 1,
+            "sort": "updated",
+        }
+        async for issues in self._fetch_data(
+            f"{self.base_url}/repos/{self._user_agent}/{repo_slug}/issues",
+            params=params,
+        ):
+            yield issues
+
+    @cache_iterator_result()
+    async def get_workflows(
+        self,
+        repo_slug: str,
+    ) -> AsyncGenerator[list[dict[str, Any]], None]:
+        """get all workflows for a repo"""
+        async for workflows in self._fetch_data(
+            f"{self.base_url}/repos/{self._user_agent}/{repo_slug}/actions/workflows",
+            values_key="workflows",
+        ):
+            logger.info(f"Found {len(workflows)} workflows for {repo_slug}")
+            yield workflows
+
+    @cache_iterator_result()
+    async def get_teams(self) -> AsyncGenerator[list[dict[str, Any]], None]:
+        """get all teams for the current user"""
+        async for teams in self._fetch_data(f"{self.base_url}/user/teams"):
+            logger.info(f"Found {len(teams)} teams for current user")
+            yield teams
+
+    @cache_iterator_result()
+    async def get_pull_requests(
+        self,
+        repo_slug: str,
+    ) -> AsyncGenerator[list[dict[str, Any]], None]:
+        """get pull requests for a repo"""
+        async for prs in self._fetch_data(
+            f"{self.base_url}/repos/{self._user_agent}/{repo_slug}/pulls"
+        ):
+            logger.info(f"Found {len(prs)} prs for {repo_slug}")
+            yield prs
--- integrations/github/main.py
+++ integrations/github/main.py
@@ -4,6 +4,7 @@ from integration.clients.github import IntegrationClient
 from integration.utils.kind import ObjectKind
 from port_ocean.context.ocean import ocean
 from port_ocean.core.ocean_types import ASYNC_GENERATOR_RESYNC_TYPE
+from port_ocean.utils.async_iterators import stream_async_iterators_tasks
 
 
 def init_client() -> IntegrationClient:
@@ -13,8 +14,17 @@ def init_client() -> IntegrationClient:
 # resync all object kinds
 @ocean.on_resync()
 async def on_resync(kind: str) -> list[dict[Any, Any]]:
-    if kind == ObjectKind.REPOSITORY:
-        resync_repositories(kind)
+    match kind:
+        case ObjectKind.REPOSITORY:
+            resync_repositories(kind)
+        case ObjectKind.ISSUE:
+            resync_issues(kind)
+        case ObjectKind.WORKFLOW:
+            resync_workflows(kind)
+        case ObjectKind.TEAM:
+            resync_teams(kind)
+        case ObjectKind.PULL_REQUEST:
+            resync_pull_requests(kind)
 
     return []
 
@@ -22,7 +32,57 @@ async def on_resync(kind: str) -> list[dict[Any, Any]]:
 # resync repository
 @ocean.on_resync(ObjectKind.REPOSITORY)
 async def resync_repositories(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
-    if kind == ObjectKind.REPOSITORY:
+    if kind != ObjectKind.REPOSITORY:
         client = init_client()
-        async for repository in client.get_repositories():
-            yield repository
+        async for repositories in client.get_repositories():
+            yield repositories
+
+
+# resync issues
+@ocean.on_resync(ObjectKind.ISSUE)
+async def resync_issues(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
+    if kind == ObjectKind.ISSUE:
+        client = init_client()
+        async for repositories in client.get_repositories():
+            tasks = [
+                client.get_issues(repo_slug=repo.get("name")) for repo in repositories
+            ]
+            async for issues in stream_async_iterators_tasks(*tasks):
+                yield issues
+
+
+# resync workflows
+@ocean.on_resync(ObjectKind.WORKFLOW)
+async def resync_workflows(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
+    if kind == ObjectKind.WORKFLOW:
+        client = init_client()
+        async for repositories in client.get_repositories():
+            tasks = [
+                client.get_workflows(repo_slug=repo.get("name"))
+                for repo in repositories
+            ]
+            async for workflows in stream_async_iterators_tasks(*tasks):
+                yield workflows
+
+
+# resync teams
+@ocean.on_resync(ObjectKind.TEAM)
+async def resync_teams(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
+    if kind == ObjectKind.TEAM:
+        client = init_client()
+        async for teams in client.get_teams():
+            yield teams
+
+
+# resync pull_requests
+@ocean.on_resync(ObjectKind.PULL_REQUEST)
+async def resync_pull_requests(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
+    if kind == ObjectKind.PULL_REQUEST:
+        client = init_client()
+        async for repositories in client.get_repositories():
+            tasks = [
+                client.get_pull_requests(repo_slug=repo.get("name"))
+                for repo in repositories
+            ]
+            async for prs in stream_async_iterators_tasks(*tasks):
+                yield prs
